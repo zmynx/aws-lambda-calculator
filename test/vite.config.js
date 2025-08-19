20e8203208b857a72cdf40b17937a078555bdb52
zMynx <lior.dux@develeap.com>
2025-08-20 01:18:30 +0300

    feat: added a vite demo site just to validate deployment

--- test/vite.config.js
+++ test/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/aws-lambda-calculator/",
   server: {
     open: true,
     port: 3001,

2e7512680f4cd5fca0cf64a76fcc9f2c3027978b
zMynx <lior.dux@develeap.com>
2025-08-21 00:46:21 +0300

    fix: merge issues

--- static-website/vite.config.ts
+++ static-website/vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
-  base: "/aws-lambda-calculator/",
-});
-
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   base: mode === 'production' 

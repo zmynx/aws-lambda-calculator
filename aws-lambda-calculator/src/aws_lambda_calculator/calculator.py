e660bbc93cfc57306ef666222835bf6233e22406
zMynx <lior.dux@develeap.com>
2025-08-29 14:34:59 +0300

    fix: unreachable loggging msg.

--- aws-lambda-calculator/src/aws_lambda_calculator/calculator.py
+++ aws-lambda-calculator/src/aws_lambda_calculator/calculator.py
@@ -92,10 +92,10 @@ def unit_conversion_ephemeral_storage(
     """
     match storage_unit:
         case "MB":
-            return ephemeral_storage_mb * 0.0009765625
             logger.debug(
                 f"Amount of ephemeral storage allocated: {ephemeral_storage_mb} MB * 0.0009765625 GB in MB = {ephemeral_storage_mb * 0.0009765625} GB"
             )
+            return ephemeral_storage_mb * 0.0009765625
         case "GB":
             return ephemeral_storage_mb
         case _:
